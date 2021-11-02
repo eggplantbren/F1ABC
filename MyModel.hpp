31e8996a26b71ef28880383f8f5e78c28b5d3c0c
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-11-02 15:55:00 +1300

    Got it to compile and link but nothing is implemented yet

--- MyModel.hpp
+++ MyModel.hpp
@@ -10,35 +10,59 @@ namespace DNest5_Template
 
 using DNest5::ParameterNames, Tools::RNG;
 
-// The "Curiously Recursive" template argument is so that the correct
-// naming scheme is used.
-class MyModel : public DNest5::UniformModel<5, MyModel>
+class MyModel
 {
     private:
 
+        std::vector<double> team_qualities;
+        std::vector<double> driver_qualities;
+
     public:
-        inline MyModel(RNG& rng);
-        inline void us_to_params();
-        inline double log_likelihood() const;
+        MyModel(RNG& rng);
+        void us_to_params();
+        double perturb(RNG& rng);
+        double log_likelihood() const;
+        std::vector<char> to_blob() const;
+        void from_blob(const std::vector<char>& blob);
+        std::string to_string() const;
+
+        static ParameterNames parameter_names;
 };
+
+
 
 /* Implementations follow */
 
-inline MyModel::MyModel(RNG& rng)
-:UniformModel(rng)
-{
-    us_to_params();
-}
+ParameterNames MyModel::parameter_names = {};
 
-inline void MyModel::us_to_params()
+inline MyModel::MyModel(RNG& rng)
 {
-    xs = us;
 }
 
 inline double MyModel::log_likelihood() const
 {
     double logl = 0.0;
     return logl;
+}
+
+double MyModel::perturb(RNG& rng)
+{
+    return 0.0;
+}
+
+std::vector<char> MyModel::to_blob() const
+{
+    return {};
+}
+
+void MyModel::from_blob(const std::vector<char>& blob)
+{
+
+}
+
+std::string MyModel::to_string() const
+{
+    return "";
 }
 
 } // namespace
